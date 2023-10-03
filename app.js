11c92b7d055c5bdb7a35ba2e2146fa3cb585ba39
IlDivinatore01 <simo.m2001@protonmail.com>
2023-10-03 14:22:34 +0200

    Syntax error

--- app.js
+++ app.js
@@ -28,20 +28,5 @@ export function initialize() {
   });
 }
 
-export function getUnitText(unit, quantity) {
-  // Restituisci il testo dell'unità corretto in base alla quantità
-  if (quantity === 1) {
-    return unit;
-  } else {
-    if (unit === "scatola") {
-      return "scatole";
-    } else if (unit === "bottiglia") {
-      return "bottiglie";
-    } else if (unit === "pacco") {
-      return "pacchi";
-    }
-  }
-}
-
 // Altre funzioni di utilità, se necessarie
 
